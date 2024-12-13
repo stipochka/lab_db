d2bf6eb6f85f2dbb7f48dfeebd690c8cc43dfc72
stipochka <drax23885@gmail.com>
2024-12-13 16:33:11 +0100

    fixed bugs with alter by id

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
-import window "lab_db/internal/window"
+import (
+	"lab_db/internal/entry_window"
+	"lab_db/internal/window"
+
+	"fyne.io/fyne/v2/app"
+)
 
 func main() {
-	win := window.New()
-	win.SetWidgets()
-	win.StartWindow("file db GUI")
+	app := app.New()
+
+	entryWin := entry_window.NewEntryWindow(app)
+
+	entryWin.OnFileSelect = func(selectedFile string) {
+		mainWin := window.New(app, selectedFile)
+
+		mainWin.Show("Table " + selectedFile)
+	}
+
+	entryWin.Show()
 }
